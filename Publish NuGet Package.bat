fb3932e3bf29d9e43e24dbb762b5bdb429ef5bca
Adam <mail@fetzenet.de>
2014-06-01 22:31:40 +0200

    Experimental NuGet Usage #ADD: Added Duality-specific NuGet files for some initial NuGet experimentation.

--- Publish NuGet Package.bat
+++ Publish NuGet Package.bat
@@ -6,8 +6,9 @@ del *.nupkg
 
 :: build the nuget packages
 .nuget\nuget pack NVorbis.nuspec
-.nuget\nuget pack NVorbis.NAudioSupport.nuspec
-.nuget\nuget pack NVorbis.OpenTKSupport.nuspec
 
 :: upload the nuget packages
 .nuget\nuget push *.nupkg
+
+:: remove nupkg files after uploading them
+del *.nupkg
